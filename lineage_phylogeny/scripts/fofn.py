4d763ee89deae4fbca515911da915df3b29905f1
Leah Roberts <lrobe09@gmail.com>
2023-01-25 11:22:13 +0000

    lineage pipeline passed testing on codon

--- lineage_phylogeny/scripts/fofn.py
+++ lineage_phylogeny/scripts/fofn.py
@@ -4,15 +4,21 @@ script that takes a directory and makes a fofn list
 
 import glob
 import os
+import sys
+
+genomes = sys.argv[1]
+ext = sys.argv[2]
+outfile = sys.argv[3]
 
 
-def make_fofn(genomes_dir, output_dir):
-    files = glob.glob(genomes_dir)
-    outputfile = output_dir + "fofn.txt"
-    with open(outputfile, "a") as fout:
+def make_fofn(genomes_dir, extension, output_file):
+    files = glob.glob(genomes_dir + "/*" + extension)
+    print(files)
+    with open(output_file, "a") as fout:
         for f in files:
+            print(f)
             sample_name = os.path.splitext(os.path.basename(f))[0]
-            fout.write("%s\%s\n" % (sample_name, f))
+            fout.write("%s\t%s\n" % (sample_name, f))
 
 
-make_fofn(snakemake.input.genomes_dir, snakemake.input.output_dir)
+make_fofn(genomes, ext, outfile)
